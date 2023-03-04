d2157a53bdc141effcc628dc3ac6f466741749f1
Panlam0405 <panlam0405@gmail.com>
2023-03-04 23:35:36 +0200

    Revert "Seardch Meal button update on comments"
    This reverts commit ff0fb40a9e6467f24f1b2346c4a4e4118d8e5cc0.

--- src/main/java/org/mealsApp/SearchMealButton.java
+++ src/main/java/org/mealsApp/SearchMealButton.java
@@ -24,7 +24,7 @@ public class SearchMealButton extends JPanel {
     //Δημιουργία constructor. Δέχεται ως είσοδο μια καρτέλα
     public SearchMealButton(JTabbedPane tabbedPane) {
         this.tabbedPane = tabbedPane;
-        setBackground(new Color(83, 83, 83,70));
+        setBackground(new Color(83, 83, 83));
     }
     //Μέθοδος κλεισίματος καρτέλας
     public void removePanel() {
@@ -34,14 +34,13 @@ public class SearchMealButton extends JPanel {
     public void openSearchField() {
         //Δημιουργία και μορφοποίηση πεδίου
         final TextField tf = new TextField();
-//        μορφωποίηση του πεδίου πριν το κλικ
         tf.setFont(new Font("Serif", Font.ITALIC, 16));
-        tf.setForeground(new Color(83, 83, 83,120));
+        tf.setForeground(new Color(100, 100, 100, 80));
         //Αν το πεδίο είναι άδειο εμφανίζεται με το μήνυμα
         if (tf.getText().isEmpty() && !(FocusManager.getCurrentKeyboardFocusManager().getFocusOwner() == tf)) {
             tf.setText("Εδώ μπορείτε να αναζητήσετε το γεύμα της αρεσκείας σας");
         }
-        //Αν επιλέξει με το caret το πεδίο αναζήτησης εξαφανίζεται το μήνυμα και αλλάζει τη γραμματοσειρά
+        //Αν επιλέξει με το caret το πεδίο αναζήτησης εξαφανίζεται το μήνυμα
         tf.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -50,24 +49,18 @@ public class SearchMealButton extends JPanel {
                 if (tf.getText().equals("Εδώ μπορείτε να αναζητήσετε το γεύμα της αρεσκείας σας")) {
                     tf.setText("");
                     tf.setFont(new Font("Serif", Font.PLAIN, 16));
-                    tf.setForeground(new Color(83, 83, 83,120));
+                    tf.setForeground(new Color(0, 0, 0));
                 }
             }
         });
         //Μορφοποίηση του πεδίου αναζήτησης γεύματος
         add(tf);
         tf.setBounds(200, 50, 500, 30);
-//        δημιουργία και προσθήκη κουμπιών για την αναζήτηση γευμάτψν ή το κλείσιμο της καρτέλας
         JButton submit = new JButton("Αναζήτηση");
         submit.setBounds(200, 100, 200, 30);
-        submit.setBackground(new Color(4, 170, 109, 197));
-        submit.setForeground(new Color(255, 255, 255));
-
         add(submit);
         JButton cancel = new JButton("Κλείσιμο");
         cancel.setBounds(500, 100, 200, 30);
-        cancel.setBackground(new Color(224, 67, 54, 197));
-        cancel.setForeground(new Color(255, 255, 255));
         add(cancel);
         setSize(700, 700);
 //        χωρίς layout manager
@@ -85,24 +78,19 @@ public class SearchMealButton extends JPanel {
 //                να γίνεται κλήση του API
 
 
-                String searchboxText = tf.getText().trim();
+                String searchboxText = tf.getText();
                 final String[] mealName = {null};
                 String mealArea ;
                 String mealInstructions ;
                 String mealCategory ;
-
 //              Δημιουργία αντικειμένου Meal για την ανάκτηση γεύματος
                 Meal meal = new Meal();
-//                αν υπάρχουν τα δεδομένα στη βάση η μεταβλητή δε θα είναι null
                 Meal existsInDb = meal.getDatafromDatabase(searchboxText);
-
-                //Έλεγχος μη-ύπαρξης του γεύματος στη Βάση Δεδομένων και εκτέλεση κώδικα για κλήση από το API
+                //Έλεγχος μη-ύπαρξης του γεύματος στη Βάση Δεδομένων
                 if (existsInDb == null) {
                     ApiCalls api = new ApiCalls();
                     JsonElement root = new JsonParser().parse(api.getMeal(searchboxText));
                     JsonElement getFirstMealProps = root.getAsJsonObject().get("meals");
-
-//                    έλεγχος αν τα δεδομένα που ήρθαν πίσω μετά την αναζήτηση δεν περιέχουν κάποιο γεύμα
                     if (!getFirstMealProps.isJsonNull()) {
                         getFirstMealProps = getFirstMealProps.getAsJsonArray().get(0);
 
@@ -111,17 +99,14 @@ public class SearchMealButton extends JPanel {
                                 .getAsString();
                     }
 
-//                    η παρακάτω ενέργεια εκτελείται αν δεν υπάρχουν δεδομένα στο json ή αν τα γεύματα που παραλαμβάνουμε είναι περισσότερα από ένα.
-//                    εμφανίζει έτσι ένα μήνυμα λάθους και προτέπει σε καινούρια αναζήτηση.
                     if (root.getAsJsonObject().get("meals").isJsonNull() ||
                             (root.getAsJsonObject().get("meals")
                                     .getAsJsonArray().size() > 1) ||
                             !searchboxText.equals(mealName[0])) {
-
-//                            αν δεν υπάρχει το editorPane το δημιουργεί και κάνει update το ui ήδη στην οθόνη κάνει update το ui
                         if (mealText == null) {
                             mealText = new JEditorPane("text/html", "");
                             mealText.setBounds(10, 250, 940, 500);
+//                            mealText.setLineWrap(true);
                             mealText.setEditable(false);
                             scroll = new JScrollPane(mealText);
                             scroll.setBounds(10, 250, 950, 500);
@@ -135,7 +120,7 @@ public class SearchMealButton extends JPanel {
                                     "<div align='center'> <font size=\"5\" ><b>Δεν υπάρχει αναζήτηση που να αντιστοιχεί στο όνομα που δώσατε στο πεδίο.<br>" +
                                     "Κάντε μία καινούρια αναζήτηση ή συμβουλευτείτε τις κατηγορίες γευμάτων στο Μενού της εφαρμογής.</b> </font></div>");
                         }
-                    } else { // αν η αναζήτηση στο Api επιστρέψει επιτυχώς ένα και μοναδικό γέυμα με βάση το όνομα που πληκτρολόγησε ο Χρήστης
+                    } else {
 
 
                         mealArea = getFirstMealProps
@@ -149,12 +134,12 @@ public class SearchMealButton extends JPanel {
                         mealCategory = getFirstMealProps
                                 .getAsJsonObject().get("strCategory")
                                 .getAsString();
-//                        Κείμεno poυ εμφανίζεται στον Χρήστη.
+
                         String text = "<b>Name :</b> <br>%s<br><br>".formatted(mealName[0]) +
                                 "<b>Category :</b> <br>%s<br><br>".formatted(mealCategory) +
                                 "<b>Area :</b> <br>%s<br><br>".formatted(mealArea) +
                                 "<b>Instructions :</b><br>%s".formatted(mealInstructions).replaceAll("\\n","<br>");
-// επικοινωνία με τη βάση για να ανεβάσει τον αριθμό των views η να ορίσει το νέο αντικείμενο στον πίνακα με αρχικό view count = 1
+
                         try {
                             EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
                             EntityManager em = emf.createEntityManager();
@@ -174,10 +159,11 @@ public class SearchMealButton extends JPanel {
                             newView.setDataBaseNewInsert(mealName[0]);
                         }
 
-//                έλεγχος αν υπάρχει το mealText(EditPane)από προηγούμενη αναζήτηση
+
                         if (mealText == null) {
                             mealText = new JEditorPane("text/html", "");
                             mealText.setBounds(10, 250, 940, 500);
+//                            mealText.setLineWrap(true);
                             mealText.setEditable(false);
                             scroll = new JScrollPane(mealText);
                             scroll.setBounds(10, 250, 950, 500);
@@ -187,26 +173,24 @@ public class SearchMealButton extends JPanel {
                         } else {
                             mealText.setText(text);
                         }
-//                        έλεγχος ύπαρξης του κουμπιού Save και προσθήκη αν δεν υπάρχει
                         if (Save == null) {
                             Save = new JButton("Αποθήκευση");
                             Save.setBounds(100, 760, 150, 40);
                             add(Save);
+
+
                         }
-//                        έλεγχος ύπαρξης του κουμπιού Modify και προσθήκη αν δεν υπάρχει
                         if (Modify == null) {
                             Modify = new JButton("Επεξεργασία");
                             Modify.setBounds(500, 760, 150, 40);
                             add(Modify);
 
                         }
-//                        έλεγχος ύπαρξης του κουμπιού Delete και προσθήκη αν δεν υπάρχει
                         if (Delete == null) {
                             Delete = new JButton("Διαγραφή");
                             Delete.setBounds(700, 760, 150, 40);
                             add(Delete);
                         }
-//   διαγραφή όποιων προηγούμενων listener είχαν τοποθετηθεί στα κουμπιά
                         for( ActionListener all : Save.getActionListeners() ) {
                             Save.removeActionListener( all );
                         }
@@ -225,16 +209,13 @@ public class SearchMealButton extends JPanel {
                         String finalMealArea = mealArea;
                         String finalMealCategory = mealCategory;
                         String finalMealInstructions = mealInstructions;
-
-//                        listener για το Save button
                         Save.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
-//                     ενναλλαγή χρηστικότητας κουμπιών μετά από το click στο save
                                 Save.setEnabled(false);
                                 Modify.setEnabled(true);
                                 Delete.setEnabled(true);
-// καταγραφή στη βάση δεδομένων όσων περιλαμβάνονται στο EditPane
+
                                 Meal meal = new Meal();
                                 meal.setMeal(finalMealName);
                                 meal.setArea(finalMealArea);
@@ -251,21 +232,19 @@ public class SearchMealButton extends JPanel {
                             }
                         });
 
-//                        listener για το Modify button
                         Modify.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
-//                         αν υπάρχει προηγούμενο instance της κλάσης που ανοίγει το παράθυρο τροποποίησης τίθεται σε null
                                 if(mod != null){
                                     System.out.println(mod);
                                     ModifyPopUpAndConfirmation.destroyInstance();
                                 }
-//                                ανοιγμα παραθύρου για την τροποίηση των στοιχείων που λάβαμε από το Api και αποθήκευση στη Βάση δεδομένων.
                                 ModifyPopUpAndConfirmation mod =ModifyPopUpAndConfirmation.getInstance(finalMealName, finalMealArea, finalMealCategory, finalMealInstructions,mealText);
+
                                 mod.makeModifications();
                             }
                         });
-//                        listener για το Modify button
+
                         Delete.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
@@ -278,7 +257,8 @@ public class SearchMealButton extends JPanel {
                         Modify.updateUI();
                     }
 
-                } else { // σε περίπτωη που υπάρχει στη βάση δεδομένων το προς αναζήτηση γευμα
+                } else {
+
                     mealName[0] = existsInDb.getMeal();
                     mealArea = existsInDb.getArea();
                     mealCategory = existsInDb.getCategory();
@@ -288,7 +268,6 @@ public class SearchMealButton extends JPanel {
                             "<b>Category :</b> <br>%s<br><br>".formatted(mealCategory) +
                             "<b>Area :</b> <br>%s<br><br>".formatted(mealArea) +
                             "<b>Instructions :</b><br>%s".formatted(mealInstructions).replaceAll("\\n","<br>");
-// επικοινωνία με τη βάση για να ανεβάσει τον αριθμό των views η να ορίσει το νέο αντικείμενο στον πίνακα με αρχικό view count = 1
                     try {
                         EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
                         EntityManager em = emf.createEntityManager();
@@ -309,10 +288,10 @@ public class SearchMealButton extends JPanel {
                         newView.setDataBaseNewInsert(mealName[0]);
                     }
 
-                    //                έλεγχος αν υπάρχει το mealText(EditPane)από προηγούμενη αναζήτηση
                     if (mealText == null) {
                         mealText = new JEditorPane("text/html", "");
                         mealText.setBounds(10, 250, 940, 500);
+//                        mealText.setLineWrap(true);
                         mealText.setEditable(false);
                         scroll = new JScrollPane(mealText);
                         scroll.setBounds(10, 250, 950, 500);
@@ -327,6 +306,7 @@ public class SearchMealButton extends JPanel {
                         Save = new JButton("Αποθήκευση");
                         Save.setBounds(100, 760, 150, 40);
                         add(Save);
+
                     }
                     if (Modify == null) {
                         Modify = new JButton("Επεξεργασία");
@@ -338,7 +318,6 @@ public class SearchMealButton extends JPanel {
                         Delete.setBounds(700, 760, 150, 40);
                         add(Delete);
                     }
-//   διαγραφή όποιων προηγούμενων listener είχαν τοποθετηθεί στα κουμπιά
                     for( ActionListener all : Save.getActionListeners() ) {
                         Save.removeActionListener( all );
                     }
@@ -383,7 +362,7 @@ public class SearchMealButton extends JPanel {
                 }
             }
         });
-//listener για κλείσιμο της καρτέλας
+
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
